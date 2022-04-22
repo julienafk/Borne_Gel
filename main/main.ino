591b176206f4f547a94ecf2fccd7a9d7462a4ed4
Djery01 <77146024+Djery01@users.noreply.github.com>
2022-04-22 10:02:22 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,96 +1,120 @@
-#include <Ultrasonic.h>
-#include <ArduinoJson.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
+#include <Ultrasonic.h>
 
 WiFiClient client;
 HTTPClient http;
+const int BATTERYPIN = 34; //pin de la batterie
+const float TensionMin = 0; //tension min
+const float TensionMax = 3.3; //tension max
+
 const char* ssid = "Eleves";  // Mettre votre SSID Wifi
 const char* password = "ml$@0931584S";
 
 
-Ultrasonic ultrasonic(2);
-int Distance;
+int getBattery() {
+  float bat = analogRead(BATTERYPIN);
+  int minValue = (4095 * TensionMin) / 3; //ESP32
+  int maxValue = (4095 * TensionMax) / 3; //ESP32
+  bat = ((bat - minValue) / (maxValue - minValue)) * 100; //mettre en pourcentage
+  if (bat > 100) { //max is 100%
+    bat = 100;
+  }
+  else if (bat < 0) { //min is 0%
+    bat = 0;
+  }
+  int valeur = bat;
+  return bat;
+}
+
+Ultrasonic ultrasonic(6);
 void setup() {
   Serial.begin(115200);   // Initialisation du moniteur série à 115200
   delay(1000);
   Serial.println("\n");
-  WiFi.begin(ssid,password);  // Initialisation avec WiFi.begin / ssid et password
+  WiFi.begin(ssid, password); // Initialisation avec WiFi.begin / ssid et password
   Serial.print("Attente de connexion ...");  // Message d'attente de connexion
-  while(WiFi.status() != WL_CONNECTED) 
-{
-  Serial.print(".");  // Affiche des points .... tant que connexion n'est pas OK
-  delay(1000);
-}
- Serial.println("\n");
- Serial.println("Connexion etablie !");  // Affiche connexion établie
- Serial.print("Adresse IP: ");
- Serial.println(WiFi.localIP());  // Affiche l'adresse IP de l'ESP32 avec WiFi.localIP
-}
-
-void loop() {
-  
-}
-//**********************************************************************************************
-int Distance = ultrasonic.read();
-/*
-  if(Distance < 10){//Capteur 1
-    Serial.println("****Distribution du gel****");//action de distribuer gel
-    delay(3500);// temps d'attente avant redistrubution
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");  // Affiche des points .... tant que connexion n'est pas OK
+    delay(1000);
   }
-  delay(1000);*/
+  Serial.println("\n");
+  Serial.println("Connexion etablie !");  // Affiche connexion établie
+  Serial.print("Adresse IP: ");
+  Serial.println(WiFi.localIP());  // Affiche l'adresse IP de l'ESP32 avec WiFi.localIP
 
-    if(Distance < 2){
-      Serial.print("**Rempli**");
-      Serial.println(Distance);
-    }
-     else if(Distance > 2 && Distance < 10){
-      Serial.print("**Bien**");
-      Serial.println(Distance);
-    }
-     else if(Distance > 10 && Distance < 15){
-      Serial.print("**Niveau Bas**");
-      Serial.println(Distance);
-    }
-     else if(Distance > 15){
-      Serial.print("**Attention**");
-      Serial.println(Distance);
-    }
-    else{
-      Serial.print("**TROP LOIN**");
-    }
+}
+void loop() {
+  //**********************************************************************************************
+  int Distance = ultrasonic.read();
+  int valGel = 0;
+
+  if (Distance < 2) {
+    Serial.print("**Rempli**");
+    Serial.println(Distance);
+    valGel = Distance;
+  }
+  else if (Distance > 2 && Distance < 10) {
+    Serial.print("**Bien**");
+    Serial.println(Distance);
+    valGel = Distance;
+  }
+  else if (Distance > 10 && Distance < 15) {
+    Serial.print("**Niveau Bas**");
+    Serial.println(Distance);
+    valGel = Distance;
+  }
+  else if (Distance > 15) {
+    Serial.print("**Attention**");
+    Serial.println(Distance);
+    valGel = Distance;
+  }
+  else {
+    Serial.print("**LOIN**");
+  }
   delay(1000);
 
-//******************************************************************************************************
-//best site ptsv2.com
-DynamicJsonDocument doc(204);
-  doc["ESP"] = 1;
-  doc["nivBatterie"] = nivBat;
-  doc["nivGel"] = Distance;
-String MonJson;
-serializeJson(doc, MonJson);
+  //******************************************************************************************************
+  int valBat = getBattery();
 
-http.begin(client, "**API-link**");
-int Statut = http.GET();
-Serial.print("Statut en attente ...");
-while(Statut != 200) 
-{
-  Serial.print(".");// Affiche des points . tant que statut n'est pas 200
-  delay(50);
+  DynamicJsonDocument doc(204);
+  doc["ESP"] = 1;
+  doc["nivBatterie"] = valBat;
+  doc["nivGel"] = valGel;
+  String MonJson;
+  serializeJson(doc, MonJson);
+
+  http.begin(client, "**API-link**");//best site ptsv2.com
+  int Statut = http.GET();
+  Serial.print("Statut en attente ...");
+  while (Statut != 200) {
+    Serial.print(".");// Affiche des points . tant que statut n'est pas 200
+    delay(50);
+  }
+  Serial.println("\n");
+  Serial.println("**ENVOYER**");
+  http.POST(MonJson);
+  DeserializationError error = deserializeJson(doc, http.getString());
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+  if (doc["succes"] == true) {
+    Serial.print("Parfait!! ");
+  }
+  delay(4000);
+  http.end();
 }
-Serial.println("\n");
-Serial.println("**ENVOYER**");
-http.POST(MonJson);
-DeserializationError error = deserializeJson(doc, http.getString());
-if (error) {
+//bool succes = doc["succes"]; // true
+//*******************************************************************************
+/*http.begin("link");
+  http.GET();
+  DeserializationError error = deserializeJson(doc, http.getString());
+  if (error) {
   Serial.print("deserializeJson() failed: ");
   Serial.println(error.c_str());
   return;
-}
-if(doc["succes"]==true){
-    Serial.print("Parfait : ");
-}
-delay(4000);
-http.end();
-}
-//bool succes = doc["succes"]; // true
+  }
+  Serial.println(doc["heure"]);//heure API*/
